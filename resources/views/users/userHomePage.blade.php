a9aabc1a262969a429a403368983bb5b7c2c0599
Blang Vibol <vibolsmart@gmail.com>
2019-08-30 17:42:19 +0700

    fix error datebase

--- resources/views/users/userHomePage.blade.php
+++ resources/views/users/userHomePage.blade.php
@@ -58,8 +58,8 @@
                             <div class="product-action">
                                 <form action="{{ route('add-wishlist') }}" method="post" id="submitWishList">
                                     @csrf
-                                    <input type="hidden" name="product_id" value="{{$productValue->id}}">
-                                    <a class="animate-left my-click" title="Wishlist" href="javascript:void(0)"
+                                    <input type="hidden" name="product_id" value="">
+                                    <a onclick="wishList({{$productValue->id}})" class="animate-left my-click" title="Wishlist" href="javascript:void(0)"
                                         id="buttonSubmitWishList">
                                         <i class="pe-7s-like"></i>
                                     </a>
@@ -369,5 +369,14 @@
                 $("#submitWishList").submit();
                 console.log("Hwllo")
             });
+</script>
+
+<script>
+    var id = null;
+
+    function wishList(id,name) {
+        document.querySelector('[name="product_id"]').setAttribute('value', id);
+    }
+
 </script>
 @endsection
