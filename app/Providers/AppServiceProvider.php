56df05d890d40ff5bbc2739056b82dbd2c4dae96
KET Van Yarath <kvanyarath@gmail.com>
2020-06-25 15:08:00 +0700

    Update "Card Header"
    Update the "Number of new Users/Product/etc".
    Hard to explain, so here's an image to illustrate: https://imgur.com/pF8Cg2i

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,6 +15,7 @@ use App\Models\Category;
 use App\User;
 use App\Request_Stock;
 use View;
+use DB;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -36,26 +37,37 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        // $sidebar = Customize::where('name','sidebar')->first();
-        // $sidebarValue = $sidebar->data;
-        // View::share('sidebar', $sidebarValue);
-        // $basicColor = Customize::where('name','basicColor')->first();
-        // $basicColorValue = $basicColor->data;
-        // View::share('basicColor', $basicColorValue);
-        // $logo = Customize::where('name','logo')->first();
-        // $logoName = $logo->data;
-        // View::share('logo', $logoName);
-        // $gradientColor = Customize::where('name','gradientColor')->first();
-        // $gradientColorValue = $gradientColor->data;
-        // View::share('gradientColor', $gradientColorValue);
-        // View::share('userData', User::all()->count());
-        // View::share('franchiseData', Franchise::all()->count());
-        // View::share('categoryData', Category::all()->count());
-        // View::share('productData', Product::all()->count());
-        // View::share('requestData', Request_Stock::where('status','pending')->count());
-        // // for no cache
-        // Cache::extend( 'none', function( $app ) {
-        //     return Cache::repository( new NullStore );
-        // } );
+        $sidebar = Customize::where('name','sidebar')->first();
+        $sidebarValue = $sidebar->data;
+        View::share('sidebar', $sidebarValue);
+        $basicColor = Customize::where('name','basicColor')->first();
+        $basicColorValue = $basicColor->data;
+        View::share('basicColor', $basicColorValue);
+        $logo = Customize::where('name','logo')->first();
+        $logoName = $logo->data;
+        View::share('logo', $logoName);
+        $gradientColor = Customize::where('name','gradientColor')->first();
+        $gradientColorValue = $gradientColor->data;
+        View::share('gradientColor', $gradientColorValue);
+        // Dashboard
+        $current_month = date('m');
+        $current_year = date('y');
+        View::share('userData', User::all()->count());
+        View::share('userDataCurrentMonth', DB::table("users")->whereBetween('created_at',["$current_year-$current_month-1","$current_year-$current_month-31"])->count());
+        
+        View::share('franchiseData', Franchise::all()->count());
+        View::share('franchiseDataCurrentMonth', DB::table("franchises")->whereBetween('created_at',["$current_year-$current_month-1","$current_year-$current_month-31"])->count());
+        
+        View::share('categoryData', Category::all()->count());
+        View::share('categoryDataCurrentMonth', DB::table("categories")->whereBetween('created_at',["$current_year-$current_month-1","$current_year-$current_month-31"])->count());
+
+        View::share('productData', Product::all()->count());
+        View::share('productDataCurrentMonth', DB::table("products")->whereBetween('created_at',["$current_year-$current_month-1","$current_year-$current_month-31"])->count());
+
+        View::share('requestData', Request_Stock::where('status','pending')->count());
+        // for no cache
+        Cache::extend( 'none', function( $app ) {
+            return Cache::repository( new NullStore );
+        } );
     }
 }
